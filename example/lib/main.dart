598a960ecafc8afccbf81da215acedf4510ae48d
Mikkel Ravn <mravn@google.com>
2017-09-20 23:33:04 +0200

    Reverted change

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,12 +10,12 @@ void main() {
               width: 280.0,
               height: 210.0,
               child: new VideoPlayer(
-                  'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4')),
+                  'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4')),
           new SizedBox(
               width: 280.0,
               height: 210.0,
               child: new VideoPlayer(
-                  'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4')),
+                  'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4')),
         ]),
       ),
     ),
